cb301dece64ec9d144357b71e00d3fbb374a42aa
Ayo Moses <ayomoses@afrimash.com>
2023-01-18 15:47:50 +0100

    Fixed linter errors

--- index.js
+++ index.js
@@ -1,61 +1,51 @@
-let books = JSON.parse(localStorage.getItem('books'))
+let books = JSON.parse(localStorage.getItem('books'));
 
 const handleStorage = (data) => {
-  localStorage.setItem('books',JSON.stringify(data))
-}
+  localStorage.setItem('books', JSON.stringify(data));
+};
 
-const listOfBooks = document.querySelector('.list')
-const formBtn = document.querySelector('.form_button')
-
-
+const listOfBooks = document.querySelector('.list');
+const formBtn = document.querySelector('.form_button');
 
 function addBook(book) {
-  books.push({id:Math.random(), ...book})
-  handleStorage(books)
+  books.push({ id: Math.random(), ...book });
+  handleStorage(books);
 }
 
 function removeBook(id) {
-  const deletedBook = document.getElementById(id)
-  books = books.filter(book =>  book.id != id)
-  deletedBook.parentElement.remove()
-  handleStorage(books)
+  const deletedBook = document.getElementById(id);
+  books = books.filter((book) => book.id !== id);
+  deletedBook.parentElement.remove();
+  handleStorage(books);
 }
 
-
-
-books.forEach(book => {
-  const listItem = document.createElement('li')
-  listItem.innerHTML= `<span class="title">${book.title}</span>
+books.forEach((book) => {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `<span class="title">${book.title}</span>
                         <span class="author">${book.author}</span>
                         <button id=${book.id} class="remove_btn">Remove</button>
                         <div class="divider"></div>
-                      `
-  listOfBooks.appendChild(listItem)
-  const listB = document.getElementById(`${book.id}`)
-  listB.addEventListener('click', () => removeBook(book.id))
-})
+                      `;
+  listOfBooks.appendChild(listItem);
+  const listB = document.getElementById(`${book.id}`);
+  listB.addEventListener('click', () => removeBook(book.id));
+});
 
-
-
-function handleSubmit(){
-  const title = document.querySelector('#book_title').value
-  const author = document.querySelector('#book_author').value
-  const index = Math.random()
-  addBook({id: index, title, author})
-  const newBook = books.filter(book => book.id === index)[0]
-  const listItem = document.createElement('li')
-  listItem.innerHTML= `<span class="title">${newBook.title}</span>
+function handleSubmit() {
+  const title = document.querySelector('#book_title').value;
+  const author = document.querySelector('#book_author').value;
+  const index = Math.random();
+  addBook({ id: index, title, author });
+  const newBook = books.filter((book) => book.id === index)[0];
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `<span class="title">${newBook.title}</span>
                           <span class="author">${newBook.author}</span>
                           <button id=${newBook.id} class="remove_btn">Remove</button>
                           <div class="divider"></div>
-                        `
-  listOfBooks.appendChild(listItem)
-  const listB = document.getElementById(`${newBook.id}`)
-  listB.addEventListener('click', () => removeBook(newBook.id))
+                        `;
+  listOfBooks.appendChild(listItem);
+  const listB = document.getElementById(`${newBook.id}`);
+  listB.addEventListener('click', () => removeBook(newBook.id));
 }
 
-formBtn.addEventListener('click', handleSubmit)
-
-
-
-
+formBtn.addEventListener('click', handleSubmit);
