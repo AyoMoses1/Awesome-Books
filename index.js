a5e7ab23182854abb6f947e6a82025c30cd0ac28
Ayo Moses <ayomoses@afrimash.com>
2023-01-19 16:00:04 +0100

    replaced book collection with javascript class

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-let books = [];
+const listOfBooks = document.querySelector('.list');
+const formBtn = document.querySelector('.form_button');
+
 
 const handleStorage = (data) => {
   localStorage.setItem('books', JSON.stringify(data));
 };
 
-const listOfBooks = document.querySelector('.list');
-const formBtn = document.querySelector('.form_button');
 
-function addBook(book) {
-  const newBook = { id: Math.random(), ...book };
-  const newArray = books.concat([newBook]);
-  handleStorage(newArray);
+class BookShelf {
+
+  constructor(){
+    this.books = [] 
+  }
+
+  addBook(book){
+    const newBook = { id: Math.random(), ...book };
+    const newArray = this.books.concat([newBook]);
+    handleStorage(newArray);
+  }
+
+  removeBook(id){
+    this.books = this.books.filter((book) => book.id !== id);
+    handleStorage(this.books);
+  }
 }
 
-function removeBook(id) {
-  const deletedBook = document.getElementById(id);
-  books = books.filter((book) => book.id !== id);
-  deletedBook.parentElement.remove();
-  handleStorage(books);
-}
 
-JSON.parse(localStorage.getItem('books'));
+let booksArray = new BookShelf()
 
 window.onload = function displayBooks() {
   const collection = JSON.parse(localStorage.getItem('books'));
-  books = collection;
+  booksArray.books = collection;
 
-  books.forEach((book) => {
+  booksArray.books.forEach((book) => {
     const listItem = document.createElement('li');
-    listItem.innerHTML = `<span class="title">${book.title}</span>
-                          <span class="author">${book.author}</span>
-                          <button id=${book.id} class="remove_btn">Remove</button>
-                          <div class="divider"></div>
+    listItem.setAttribute('id', book.id)
+    listItem.innerHTML = `<div>
+                            <span class="title">"${book.title}"</span> by <span class="author">${book.author}</span>
+                          </div>
+                          <div>
+                            <button id=${book.id} class="remove_btn">Remove</button>
+                          </div>
                         `;
     listOfBooks.appendChild(listItem);
     const listB = document.getElementById(`${book.id}`);
-    listB.addEventListener('click', () => removeBook(book.id));
+    listB.addEventListener('click', () => {
+      const deletedBook = document.getElementById(book.id);
+      deletedBook.parentNode.removeChild(deletedBook);
+      booksArray.removeBook(book.id)
+    });
   });
 };
 
@@ -43,19 +56,26 @@ function handleSubmit() {
   const title = document.querySelector('#book_title').value;
   const author = document.querySelector('#book_author').value;
   const index = Math.random();
-  addBook({ id: index, title, author });
+  booksArray.addBook({ id: index, title, author });
   const collection = JSON.parse(localStorage.getItem('books'));
-  books = collection;
-  const newBook = books.filter((book) => book.id === index)[0];
+  booksArray = collection;
+  const newBook = booksArray.filter((book) => book.id === index)[0];
   const listItem = document.createElement('li');
-  listItem.innerHTML = `<span class="title">${newBook.title}</span>
-                          <span class="author">${newBook.author}</span>
-                          <button id=${newBook.id} class="remove_btn">Remove</button>
-                          <div class="divider"></div>
+  listItem.setAttribute('id', newBook.id)
+  listItem.innerHTML = `<div>
+                            <span class="title">"${newBook.title}"</span> by <span class="author">${newBook.author}</span>
+                          </div>
+                          <div>
+                            <button id=${newBook.id} class="remove_btn">Remove</button>
+                          </div>
                         `;
   listOfBooks.appendChild(listItem);
   const listB = document.getElementById(`${newBook.id}`);
-  listB.addEventListener('click', () => removeBook(newBook.id));
+  listB.addEventListener('click', () => {
+    const deletedBook = document.getElementById(newBook.id);
+    deletedBook.parentNode.removeChild(deletedBook);
+    booksArray.removeBook(newBook.id)
+  });
 }
 
 formBtn.addEventListener('click', handleSubmit);
